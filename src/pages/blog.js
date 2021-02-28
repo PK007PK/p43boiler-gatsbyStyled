9ea16de4f16a8fd278a6400044f7a80f0984eae7
Piotr Krasny <krasny.piotr@gmail.com>
2021-02-28 13:53:37 +0100

    Add displying  blog posts to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 import AppProvider from "../AppProvider";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
 
 const BlogPage = ({ location, data }) => {
+  const posts = data.allMarkdownRemark.nodes;
+
+  const DisplayPosts = () => (
+    <ul style={{ listStyle: `none`, paddingLeft: 0 }}>
+      {posts.map((post) => {
+        return (
+          <li key={post.fields.slug}>
+            <h3>{post.frontmatter.title}</h3>
+            <p>{post.frontmatter.description}</p>
+            <Link to={post.fields.slug} itemProp="url">
+              More
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
   return (
     <AppProvider state={location?.state}>
       <Layout>
         <h1>Blog page</h1>
-        <Button />
+        <DisplayPosts />
       </Layout>
     </AppProvider>
   );
@@ -47,13 +65,6 @@ export const pageQuery = graphql`
         }
       }
     }
-    # placeholderImage: file(relativePath: { eq: "pkinf1.jpg" }) {
-    #   childImageSharp {
-    #     fluid(maxWidth: 300) {
-    #       ...GatsbyImageSharpFluid
-    #     }
-    #   }
-    # }
   }
 `;
 
