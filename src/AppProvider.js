e62764305c13e126c5eea087aee6ee9a512bc9f5
Piotr Krasny <krasny.piotr@gmail.com>
2021-03-08 12:21:33 +0100

    Add theme to AppProvider

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -10,8 +10,29 @@ export function AppProvider({ children }) {
   const piotrek = "Łotrek";
 
   return (
-    <AppContext.Provider value={{ piotrek }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ piotrek }}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </AppContext.Provider>
   );
 }
 
 export default AppContext;
+
+// const [isActive, setIsActive] = useState(false);
+
+// const passStateBetweenSites = () => {
+//   state !== null && setIsActive(state.isActive);
+// };
+
+// useEffect(() => passStateBetweenSites(), []);
+
+// const toogleIsActive = () => setIsActive((prevValue) => !prevValue);
+
+// return (
+//   <AppContext.Provider value={{ isActive, toogleIsActive }}>
+//     <ThemeProvider theme={theme}>{children}</ThemeProvider>
+//   </AppContext.Provider>
+// );
+// };
+
+// export default AppProvider;
