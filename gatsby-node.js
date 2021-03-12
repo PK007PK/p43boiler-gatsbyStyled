2157509828987200156bd81aa465da513e3d746d
Piotr Krasny <krasny.piotr@gmail.com>
2021-03-12 12:00:19 +0100

    Save

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,8 @@ import fetch from 'isomorphic-fetch';
 async function turnPostsIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
   const postTemplate = path.resolve('./src/templates/blog-post.js');
-  // 2. Query all pizzas
+
+  // 2. Query all posts
   const { data } = await graphql(`
     query {
       allMarkdownRemark {
@@ -19,7 +20,8 @@ async function turnPostsIntoPages({ graphql, actions }) {
       }
     }
   `);
-  // 3. Loop over each pizza and create a page for that pizza
+
+  // 3. Loop over each post and create a page for that post
 
   data.allMarkdownRemark.nodes.forEach((page) => {
     const slug = slugify(
@@ -39,7 +41,4 @@ export async function createPages(params) {
   // Create pages dynamically
   // Wait for all promises to be resolved before finishing this function
   await Promise.all([turnPostsIntoPages(params)]);
-  // 1. Pizzas
-  // 2. Toppings
-  // 3. Slicemasters
 }
