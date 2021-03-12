146c5a06ad28a12bd1bca375e8383139f017a066
Piotr Krasny <krasny.piotr@gmail.com>
2021-03-12 09:34:01 +0100

    Save

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,66 +1,66 @@
-import React, { useContext } from 'react';
-import { graphql, Link } from 'gatsby';
-import AppContext from '../AppProvider';
-import Layout from '../components/Layout';
-import SEO from '../components/SEO';
+// import React, { useContext } from 'react';
+// import { graphql, Link } from 'gatsby';
+// import AppContext from '../AppProvider';
+// import Layout from '../components/Layout';
+// import SEO from '../components/SEO';
 
-const BlogPage = ({ data }) => {
-  const posts = data.allMarkdownRemark.nodes;
-  const DisplayPosts = () => (
-    <ul style={{ listStyle: `none`, paddingLeft: 0 }}>
-      {posts.map((post) => (
-        <li key={post.fields.slug}>
-          <h3>{post.frontmatter.title}</h3>
-          <p>{post.frontmatter.description}</p>
-          <Link to={post.fields.slug} itemProp="url">
-            More
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+// const BlogPage = ({ data }) => {
+//   const posts = data.allMarkdownRemark.nodes;
+//   const DisplayPosts = () => (
+//     <ul style={{ listStyle: `none`, paddingLeft: 0 }}>
+//       {posts.map((post) => (
+//         <li key={post.fields.slug}>
+//           <h3>{post.frontmatter.title}</h3>
+//           <p>{post.frontmatter.description}</p>
+//           <Link to={post.fields.slug} itemProp="url">
+//             More
+//           </Link>
+//         </li>
+//       ))}
+//     </ul>
+//   );
 
-  return (
-    <Layout>
-      <SEO title="Blog page" />
-      <h1>Blog page</h1>
-      <DisplayPosts />
-    </Layout>
-  );
-};
+//   return (
+//     <Layout>
+//       <SEO title="Blog page" />
+//       <h1>Blog page</h1>
+//       <DisplayPosts />
+//     </Layout>
+//   );
+// };
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/blog/.*.md$/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "YYYY-MM-DD")
-          title
-          description
-          #   tags
-          #   thumbnail {
-          #     childImageSharp {
-          #       fluid {
-          #         ...GatsbyImageSharpFluid_tracedSVG
-          #       }
-          #     }
-          #   }
-        }
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(
+//       filter: { fileAbsolutePath: { regex: "/blog/.*.md$/" } }
+//       sort: { fields: [frontmatter___date], order: DESC }
+//     ) {
+//       nodes {
+//         excerpt
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           date(formatString: "YYYY-MM-DD")
+//           title
+//           description
+//           #   tags
+//           #   thumbnail {
+//           #     childImageSharp {
+//           #       fluid {
+//           #         ...GatsbyImageSharpFluid_tracedSVG
+//           #       }
+//           #     }
+//           #   }
+//         }
+//       }
+//     }
+//   }
+// `;
 
-export default BlogPage;
+// export default BlogPage;
