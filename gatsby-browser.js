ad3d591304b80e7a8a158b69e54b21543288a5bb
Piotr Krasny <krasny.piotr@gmail.com>
2021-03-17 17:49:53 +0100

    Change font-face placement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './src/styles/index.css';
+
 import { AppProvider } from './src/AppProvider';
 
 export function wrapRootElement({ element }) {
--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -1,18 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
-import font from '../assets/fonts/frenchfries.woff';
-
 const Typography = createGlobalStyle`
-  @font-face {
-    font-family: FrenchFries;
-    src: url(${font});
-    font-display: swap;
-  }
 
-  html {
-    font-family: FrenchFries, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: var(--black);
-  }
 
   p, li {
     letter-spacing: 0.5px;
